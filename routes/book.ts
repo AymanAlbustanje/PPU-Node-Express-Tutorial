cfd605d3a25e4c3351d371c949732c1cfe0995b1
AymanAlbustanje <201125@ppu.edu.ps>
2024-07-23 08:04:20 -0900

    .

--- routes/book.ts
+++ routes/book.ts
@@ -6,7 +6,27 @@ const router = Router()
 
 router.get("/filter-arr", filterArrayController)
 router.get("/",logRequestMiddleware,  getAllBooks)
-router.post("/", createBook)
+router.post("/", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        if (!req.body.bookName || !req.body.author) {
+            return res.status(400).json({
+                message: "some fields are missing!!",
+                success: false
+            })
+        }
+
+        const book = await createBook(req.body)
+
+        res.json({
+            message: "book created successfully",
+            book: book
+        })
+    } catch (error) {
+        console.log("error: " + error);
+        next(error)
+    }
+})
 router.delete("/:id", deleteBook)
 router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -22,7 +42,7 @@ router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
         })
     } catch (error) {
         console.log("error: " + error);
-        next()
+        next(error)
     }
 })
 
